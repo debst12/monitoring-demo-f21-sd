2beeccb978ecd18d241c9e2234252a500052989a
Spencer DeBrine <debst12@gmail.com>
2022-04-28 14:44:47 -0600

    Final Commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.json())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -16,15 +17,60 @@ var rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../index.html'))
-// })
-
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
     rollbar.info('html file served successfully')
 })
 
+app.get('/styles', (req,res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    res.status(200).send(students)
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+
+    res.status(200).send(students)
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
+
 const port = process.env.PORT || 4545
+app.use(rollbar.errorHandler())
+
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
