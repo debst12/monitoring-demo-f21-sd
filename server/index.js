04e453dbf0139cd90944e9a9ebcad8f277fc7f8a
Spencer DeBrine <debst12@gmail.com>
2022-04-28 14:24:10 -0600

    added rollbar stuff

--- server/index.js
+++ server/index.js
@@ -20,6 +20,11 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served successfully')
+})
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
